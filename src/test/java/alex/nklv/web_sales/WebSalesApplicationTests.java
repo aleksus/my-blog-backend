e7d997febb58d42c01bcefb9bc3b3a9df2e0b954
Alexander Nikolaev <alexnxg@gmail.com>
2026-03-22 23:11:07 +0200

    add project backend

---
+++ src/test/java/alex/nklv/web_sales/WebSalesApplicationTests.java
@@ -0,0 +1,13 @@
+package alex.nklv.web_sales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
